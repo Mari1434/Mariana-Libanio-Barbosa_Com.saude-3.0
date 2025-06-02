44508620099858838476b298cb4a10a9b3f503cc
Mariana Libânio Barbosa <62571672+Mari1434@users.noreply.github.com>
2025-06-02 11:39:25 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDLa-GN7pV45BCwgqU4kVeHTrso7eIstug",
-  authDomain: "comsaude3-fd868.firebaseapp.com",
-  projectId: "comsaude3-fd868",
-  storageBucket: "comsaude3-fd868.firebasestorage.app",
-  messagingSenderId: "726228261307",
-  appId: "1:726228261307:web:01656ee41ebd3a01f84c9e",
-  measurementId: "G-XVCP2M8WW5"
+
 };
 
 
